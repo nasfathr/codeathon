d82378a7207936fdcd76580e71327dc2a91619b2
Adly Abdullah <slebetman@gmail.com>
2017-11-19 13:06:29 +0800

    Initial implementation of API

---
+++ web/main.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const blacklist = require('./lib/blacklist');
+const suspectlist = require('./lib/suspectlist');
+const whitelist = require('./lib/whitelist');
+
+const app = express();
+
+app.get('/api/ethereum/check/:hash', (req, res) => {
+	var address = request.params.hash;
+	
+	whitelist.check(address).then(status => {
+		if (status) {
+			res.json({good: true, bad: false, blocked: false, suspect: false, whitelist: true});
+		}
+		else {
+			blacklist.check(address).then(status => {
+				if (status) {
+					res.json({good: false, bad: true, blocked: true, suspect: false});
+				}
+				else {
+					suspectlist.check(address).then(status => {
+						if (status) {
+							res.json({good: false, bad: true, blocked: false, suspect: true});
+						}
+						else {
+							res.json({good: true, bad: false, blocked: false, suspect: false});
+						}
+					});
+				}
+			});
+		}
+	});
+});
+
+app.get('/api/ethereum/trust/:hash', (req, res) => {
+	var address = request.params.hash;
+	
+	suspectlist.check(address).then(status => {
+		if (status) {
+			suspectlist.upvote(address);
+			res.json({status: 'OK'});
+		}
+	});
+});
+
+app.listen(3333, () => console.log('Example app listening on port 3000!'));
